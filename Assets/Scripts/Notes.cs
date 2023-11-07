7ec0d7e0266bceb80232115727c60396dd3d0126
Scayalet <270301797@yoobeestudent.ac.nz>
2023-11-07 21:38:09 +1300

    Orbs Scripted
    Audio and visual feedback of picking up orbs scripted. Next is inventory and interaction with holders.

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    public AudioSource c;
+    public AudioSource d;
+    public AudioSource e;
+
+    private void Start()
+    {
+        
+    }
+}
--- Assets/Scripts/OrbController.cs
+++ Assets/Scripts/OrbController.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class OrbController : MonoBehaviour
 {
+
+    public Notes notes;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            if (this.tag == "orbPurple")
+            {
+                notes.c.Play();
+            }
+            else if (this.tag == "orbAqua")
+            {
+                notes.d.Play();
+            }
+            else if (this.tag == "orbBlue")
+            {
+                notes.e.Play();
+            }
+
             Destroy(gameObject);
         }
     }
